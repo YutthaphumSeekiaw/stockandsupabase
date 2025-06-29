e6c7367a6355676f944b416f3d651b243328ef37
yutthaphumseekiaw <yutthaphum_bo@hotmail.com>
2025-06-29 21:56:22 +0700

    cvbb

--- src/app/supabase-client.js
+++ src/app/supabase-client.js
@@ -3,6 +3,7 @@ import { createClient } from "@supabase/supabase-js";
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
 const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
 
+supabaseKey = "eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImprcWd6bm"
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 export default supabase;
